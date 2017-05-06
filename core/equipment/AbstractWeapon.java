d805bcd6bef6c60ac6c11d73135df82f801c556a
PetrMat <petr.matras@gmail.com>
2017-05-06 23:44:51 +0200

    uprava tridy abstractWeapon

--- core/equipment/AbstractWeapon.java
+++ core/equipment/AbstractWeapon.java
@@ -1,11 +1,22 @@
 package core.equipment;
 
 public abstract class AbstractWeapon {
-	
-	private long attack;
+/* je to cele blbost, mel bych spis udelat jednu tridu 
+ * weapon, ktere budu prirazovat z vyctu vlastnosti
+ * jako jmeno, silu a specialni vlastnosti
+ * */ 	
+	private long attackWeapon;
 
 	public AbstractWeapon() {
 		
+	}
+
+	protected long getAttackWeapon() {
+		return attackWeapon;
+	}
+
+	protected void setAttackWeapon(long attackWeapon) {
+		this.attackWeapon = attackWeapon;
 	}
 
 }
